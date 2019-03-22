4da193059c3ba45161f59ce08fe3d9f9c5523900
KrittiyaB <krittiya_b@sci.tu.ac.th>
2019-03-22 15:44:04 +0700

    send assignment

---
+++ booking.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"golang.org/x/net/context"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Booking struct {
+	Id    string    `bson:"id, json:"id"`
+	Name  string    `bson:"name",json:"name"`
+	Room  string    `bson:"room",json:"room"`
+	Start time.Time `bson:"start",json:"start"`
+	End   time.Time `bson:"end",json:"end"`
+}
+
+func connectDB() *mongo.Collection {
+	mongoURI := "mongodb://localhost:27017"
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	err = client.Ping(context.TODO(), readpref.Primary())
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	collection := client.Database("Library").Collection("Booking")
+	return collection
+}
+
+func genUUID() string {
+	n := 4
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		panic(err)
+	}
+	s := fmt.Sprintf("%X", b)
+	return s
+}
+
+func booking(c *gin.Context) {
+	log.Println("==> booking")
+	ctx := c.Request.Context()
+
+	var booking Booking
+	if err := c.Bind(&booking); err != nil {
+		return
+	}
+	log.Printf("%+v\n", booking.Name)
+
+	booking.Id = genUUID()
+	collection := connectDB()
+	res, err := collection.InsertOne(ctx, booking)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("return ---> %+v\n", res.InsertedID)
+	c.JSON(http.StatusOK, booking)
+}
+
+func findBooking(c *gin.Context) {
+	log.Println("==> Find Booking")
+
+	collection := connectDB()
+	ctx := context.TODO()
+	cur, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+	defer cur.Close(ctx)
+	var books []Booking
+	for cur.Next(ctx) {
+		var book Booking
+		if err := cur.Decode(&book); err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		books = append(books, book)
+		fmt.Printf("%+v\n", book)
+	}
+	c.JSON(http.StatusOK, books)
+}
+
+func findBookingById(c *gin.Context) {
+	log.Println("==> Find Booking by Id")
+	ctx := c.Request.Context()
+	id := c.Param("id")
+
+	collection := connectDB()
+	cur, err := collection.Find(ctx, bson.M{"id": id})
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+	defer cur.Close(ctx)
+	var result bson.M
+	for cur.Next(ctx) {
+
+		if err := cur.Decode(&result); err != nil {
+		}
+		fmt.Printf("%+v\n", result)
+	}
+	c.JSON(http.StatusOK, result)
+}
+
+func cancelBooking(c *gin.Context) {
+	log.Println("==> Cancel Booking")
+	ctx := c.Request.Context()
+	id := c.Param("id")
+	collection := connectDB()
+	res, err := collection.DeleteOne(ctx, bson.D{{"id", id}})
+
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	if res.DeletedCount == 0 {
+		c.Status(http.StatusNotFound)
+		return
+	}
+}
+
+func main() {
+	router := gin.Default()
+	book := router.Group("/bookings")
+	{
+		book.POST("/", booking)
+		book.GET("/", findBooking)
+		book.GET("/:id", findBookingById)
+		book.DELETE("/:id", cancelBooking)
+	}
+	router.Run()
+}
